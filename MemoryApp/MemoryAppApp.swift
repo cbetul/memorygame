69fa7f329b07a158514119d86e5d45aed90e9c75
cbetul <cimendag16@itu.edu.tr>
2022-02-17 11:33:26 +0300

    Initial Commit

---
+++ MemoryApp/MemoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryAppApp.swift
+//  MemoryApp
+//
+//  Created by Betül Çimendağ on 17.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
